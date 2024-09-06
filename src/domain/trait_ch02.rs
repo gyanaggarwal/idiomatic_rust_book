6d3594712b0cf8dbc436d33b58f3198134f455bf
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2024-09-06 05:06:58 -0500

    trait with generic

--- src/domain/trait_ch02.rs
+++ src/domain/trait_ch02.rs
@@ -42,3 +42,55 @@ impl Ch02Trait for Crdt<EWFlag> {
         println!("EWFlag: {:?}", self.name);
     }
 }
+
+pub trait Ch02TraitGeneric<T> {
+    fn gfun01(&self) -> T;
+    fn gfun02(&self, alt_name: T) -> T {
+        alt_name
+    }
+    fn gfun03(&self) -> T;
+    fn gfun04(&self, alt_name: T) -> Vec<T> {
+        vec![self.gfun01(), self.gfun02(alt_name), self.gfun03()]
+    }
+}
+
+#[derive(Clone)]
+pub struct CRDT<T, State> {
+    pub name: T,
+    pub alt_name: T,
+    pub f_name: T,
+    pub state: std::marker::PhantomData<State>
+}
+
+impl <T: Clone, State> CRDT<T, State> {
+    pub fn new(name: T, alt_name: T, f_name: T) -> Self {
+        Self{name, alt_name, f_name, state: std::marker::PhantomData::<State>}
+    }
+
+    pub fn final_name(&self) -> T {
+        self.f_name.clone()
+    }
+}
+
+impl <T: Clone> Ch02TraitGeneric<T> for CRDT<T, EWFlag> {
+    fn gfun01(&self) -> T {
+        self.name.clone()
+    }
+    fn gfun03(&self) -> T {
+        self.final_name()
+    }
+}
+
+impl <T: Clone> Ch02TraitGeneric<T> for CRDT<T, DWFlag> {
+    fn gfun01(&self) -> T {
+        self.name.clone()
+    }
+
+    fn gfun02(&self, _alt_name: T) -> T {
+        self.alt_name.clone()
+    }
+    
+    fn gfun03(&self) -> T {
+        self.final_name()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use idiomatic_rust::domain::trait_ch02::{Crdt, EWFlag, DWFlag, Ch02Trait};
+use idiomatic_rust::domain::trait_ch02::{Crdt, CRDT, 
+                                         EWFlag, DWFlag, 
+                                         Ch02Trait, Ch02TraitGeneric};
 
 fn main() {
     let crdt1: Crdt<EWFlag> = Crdt::new("EWFlag01".to_owned());
@@ -7,4 +9,15 @@ fn main() {
     crdt1.func04();
     crdt2.func04();
 
+    let crdt3: CRDT<String, EWFlag> = CRDT::new("EWF.name".to_owned(),
+                                                "EWF.alt_name".to_owned(),
+                                                "EWF.final_name".to_owned());
+    let crdt4: CRDT<String, DWFlag> = CRDT::new("DWF.name".to_owned(),
+                                                "DWF.alt_name".to_owned(),
+                                                "DWF.final_name".to_owned());
+    let r3 = crdt3.gfun04("XXX.new_name".to_owned());
+    let r4 = crdt4.gfun04("YYY.new_name".to_owned());
+
+    println!("r3 {:?}", r3);
+    println!("r4 {:?}", r4);
 }
